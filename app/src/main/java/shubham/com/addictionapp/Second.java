dff2e4dc7142b097dfe790de57a20184977f7d92
Dhawal <dhawaljaiswal11@gmail.com>
2018-07-03 12:08:25 +0530

    Wireframe Dashboard Creation using Activity

--- app/src/main/java/shubham/com/addictionapp/Second.java
+++ app/src/main/java/shubham/com/addictionapp/Second.java
@@ -1,13 +1,28 @@
 package shubham.com.addictionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Second extends AppCompatActivity {
+
+    Button Dash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Dash = (Button)findViewById(R.id.button);
+        Dash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Second.this, Dashboard.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
