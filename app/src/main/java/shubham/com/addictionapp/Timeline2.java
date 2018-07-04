e4d838bbc32fc79b6afc1f97881498c10e6924b3
Dhawal <dhawaljaiswal11@gmail.com>
2018-07-04 12:03:12 +0530

    Wireframe Dashboard Creation using Activity

---
+++ app/src/main/java/shubham/com/addictionapp/Timeline2.java
@@ -0,0 +1,13 @@
+package shubham.com.addictionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Timeline2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline2);
+    }
+}
