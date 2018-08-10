b260fcb19484a74daa4766045389086c9b50ecca
Dhawal <dhawaljaiswal11@gmail.com>
2018-08-11 02:13:53 +0530

    UPdate Dashboard

---
+++ app/src/main/java/shubham/com/addictionapp/Timeline2.java
@@ -0,0 +1,13 @@
+package shubham.com.addictionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Timeline2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline2);
+    }
+}
